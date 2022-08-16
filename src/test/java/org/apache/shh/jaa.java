36c6fe8e84a79c4856c84ffb3226c2bf999d4d10
unknown <Susmita.Chougule@mindtree.com>
2022-08-16 14:34:37 +0530

    1st commit

---
+++ src/test/java/org/apache/shh/jaa.java
@@ -0,0 +1,12 @@
+package org.apache.shh;
+
+import org.junit.Test;
+
+public class jaa {
+	@Test
+	public void i()
+	{
+		System.out.println("we will go to oyo on 21");
+	}
+
+}
---
+++ src/test/java/org/apache/shh/oo.java
@@ -0,0 +1,12 @@
+package org.apache.shh;
+
+import org.junit.Test;
+
+public class oo {
+	@Test
+	public void go()
+	{
+		System.out.println("HIEEE");
+	}
+
+}
